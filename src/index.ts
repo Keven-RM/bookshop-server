4ad26dc0166d767a86df1d67d1512bd77fe72181
=Keven-RM <=keven.rm2004@gmail.com>
2022-10-11 14:13:18 -0300

    update on cors config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request } from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import cors from "cors";
 import router  from './routes';
 import * as dotenv from "dotenv";
@@ -6,8 +6,17 @@ dotenv.config({path: __dirname + '/.env'});
 
 const app = express();
 
+var allowCrossDomain = function(req: Request, res: Response, next: NextFunction) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(allowCrossDomain)
 app.use(router);
-app.use(cors<Request>);
+app.use(cors());
 app.use(express.json());
 app.listen(process.env.SERVERPORT || 8080);
 console.log('> Server is on');
